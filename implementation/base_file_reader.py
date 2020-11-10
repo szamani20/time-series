536f9acbf0381ff1f8053ee3f3013dc21acca652
szamani20 <szchem20@gmail.com>
2020-11-10 12:02:07 -0500

    Added window based anomaly injection

--- implementation/base_file_reader.py
+++ implementation/base_file_reader.py
@@ -71,23 +71,45 @@ class FileReader:
         return df
 
     @staticmethod
-    def inject_anomaly(dataframes, rate=.05, max_anomaly_size=3):
+    def inject_anomaly(dataframes, rate=.01, max_anomaly_size=3):
+        window_size = int(1 / rate)
+
         for df in dataframes:
             is_anomaly = [0] * df.shape[0]
-            max_value = df[VALUE_COLUMN].max()
-            anomaly_values = [round(max_value * 0.8), round(max_value * 0.9),
-                              round(max_value * 1), round(max_value * 1.1), round(max_value * 1.2)]
-            anomaly_counts = int(df.shape[0] * rate)
-            for i in range(anomaly_counts):
+            # not having an anomaly in the first 5 windows
+            exception_window_size = int((WINDOW_EXCEPTION_RATE * df.shape[0]) / window_size)
+            anomaly_counts = int(df.shape[0] * rate) - exception_window_size
+            for i in range(exception_window_size, anomaly_counts + exception_window_size):
+                max_window_value = df[VALUE_COLUMN][i * window_size: (i + 1) * window_size].max()
+                anomaly_values = [round(max_window_value * 1.1), round(max_window_value * 1.2),
+                                  round(max_window_value * 1.3), round(max_window_value * 1.4)]
                 anomaly_size = random.randint(1, max_anomaly_size)
-                anomaly_start_index = min(i * anomaly_counts + random.randint(1, anomaly_counts),
-                                          df.shape[0] - max_anomaly_size)
+                anomaly_start_index = i * window_size + random.randint(1, window_size - anomaly_size - 1)
                 anomaly_end_index = anomaly_start_index + anomaly_size
-
                 is_anomaly[anomaly_start_index: anomaly_end_index] = [1] * anomaly_size
                 df.iloc[anomaly_start_index: anomaly_end_index, 1] = random.choices(anomaly_values,
                                                                                     k=anomaly_size)
-
             df['is_anomaly'] = is_anomaly
 
         return dataframes
+
+        # for i in range(len(dataframes)):
+        #     df = dataframes[i]
+        #     is_anomaly = [0] * df.shape[0]
+        #     max_value = df[VALUE_COLUMN].max()
+        #     anomaly_values = [round(max_value * 0.8), round(max_value * 0.9),
+        #                       round(max_value * 1), round(max_value * 1.1), round(max_value * 1.2)]
+        #     anomaly_counts = int(df.shape[0] * rate)
+        #     for i in range(anomaly_counts):
+        #         anomaly_size = random.randint(1, max_anomaly_size)
+        #         anomaly_start_index = min(i * anomaly_counts + random.randint(1, anomaly_counts),
+        #                                   df.shape[0] - max_anomaly_size)
+        #         anomaly_end_index = anomaly_start_index + anomaly_size
+        #
+        #         is_anomaly[anomaly_start_index: anomaly_end_index] = [1] * anomaly_size
+        #         df.iloc[anomaly_start_index: anomaly_end_index, 1] = random.choices(anomaly_values,
+        #                                                                             k=anomaly_size)
+        #
+        #     df['is_anomaly'] = is_anomaly
+        #
+        # return dataframes
